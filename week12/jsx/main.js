81a6650ce822103ef750fb4793a09e39ae1159a8
briskr <briskr@126.com>
2020-09-17 17:38:39 +0800

    实现JSX转换成DOM节点

--- week12/jsx/main.js
+++ week12/jsx/main.js
@@ -1,5 +1,24 @@
-for (const i of [1, 2, 3]) {
-  console.log(i);
+function createElement(tag, attrs, ...children) {
+  const elem = document.createElement(tag);
+  for (const key in attrs) {
+    elem.setAttribute(key, attrs[key]);
+  }
+  for (const child of children) {
+    /* if (typeof child === 'string') {
+      child = document.createTextNode(child);
+    }
+    elem.appendChild(child); */
+    // append 方法可接受 DOMString 或 Node
+    elem.append(child);
+  }
+  return elem;
 }
 
-let a = <div />;
+let a = (
+  <div class="cls1">
+    text<span>label1</span>
+    <span>label2</span>
+  </div>
+);
+
+document.body.appendChild(a);
