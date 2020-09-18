071c8926e9e2dcf17b23a49f00d0da2b7824ed24
briskr <briskr@126.com>
2020-09-18 19:04:34 +0800

    实现图片单向滑动的轮播效果

--- week12/jsx/main.js
+++ week12/jsx/main.js
@@ -17,12 +17,30 @@ class Carousel extends Component {
       this.root.appendChild(child);
     }
 
-    let current = 0;
+    let currentIndex = 0;
     setInterval(() => {
-      current = ++current % this.root.children.length;
-      for (const child of this.root.children) {
-        child.style.transform = `translateX(-${current * 100}%)`;
-      }
+      // nextIndex 的值可以表示平移后的位置应当向左偏移几个 100%, 按 1, 2, 3, 0, 1,... 循环变化
+      // 即平移动作的目标位置是 -nextIndex * 100%, 由此反推平移动作的起始位置是 (-nextIndex + 1) * 100%
+      const nextIndex = (currentIndex + 1) % this.root.children.length;
+
+      const currentElm = this.root.children[currentIndex];
+      const nextElm = this.root.children[nextIndex];
+
+      // 禁用 transition 滑动效果，先把下一帧图片放置到平移动作的起始位置
+      nextElm.style.transition = 'none';
+      nextElm.style.transform = `translateX(${(-nextIndex + 1) * 100}%)`;
+
+      // 随后开始执行滑动平移动作
+      setTimeout(() => {
+        // 恢复 transition 滑动效果
+        nextElm.style.transition = '';
+        // 一起移动前后相邻的两帧图片
+        // 旧的当前帧，滑动平移目标位置是目前的位置 (-currentIndex * 100%) 再向左 100%
+        currentElm.style.transform = `translateX(${(-currentIndex - 1) * 100}%)`;
+        nextElm.style.transform = `translateX(${-nextIndex * 100}%)`;
+
+        currentIndex = nextIndex;
+      }, 16 /* 大致是浏览器动画帧间隔时间，未采用 requestAnimationFrame 实现，课内未详细分析，只提到那样会需要两次 RAF 动作 */);
     }, 3000);
 
     return this.root;
