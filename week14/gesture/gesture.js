bc1fcbbbe10931e59f44b2458e0608bdf1e64c43
briskr <briskr@126.com>
2020-10-11 12:28:44 +0800

    分析事件时序，处理不同类型的动作

--- week14/gesture/gesture.js
+++ week14/gesture/gesture.js
@@ -38,15 +38,58 @@ elem.addEventListener('touchcancel', (event) => {
   }
 });
 
+let pressTimeoutHandle;
+let startX, startY;
+let isPan = false,
+  isTap = true,
+  isPress = false;
+
 const start = (point) => {
   console.log('start', point.clientX, point.clientY);
+  (startX = point.clientX), (startY = point.clientY);
+  isTap = true;
+  isPan = false;
+  isPress = false;
+
+  pressTimeoutHandle = setTimeout(() => {
+    isTap = false;
+    isPan = false;
+    isPress = true;
+    console.log('press');
+    pressTimeoutHandle = null;
+  }, 500);
 };
 const move = (point) => {
-  console.log('move', point.clientX, point.clientY);
+  let dx = point.clientX - startX,
+    dy = point.clientY - startY;
+  if (!isPan && dx ** 2 + dy ** 2 > 100) {
+    isTap = false;
+    isPan = true;
+    isPress = false;
+    console.log('panstart');
+    clearTimeout(pressTimeoutHandle);
+  }
+  if (isPan) {
+    console.log('panning', dx, dy);
+    console.log('pan');
+  }
+  //console.log('move', point.clientX, point.clientY);
 };
 const end = (point) => {
-  console.log('end', point.clientX, point.clientY);
+  if (isTap) {
+    console.log('tap');
+    clearTimeout(pressTimeoutHandle);
+  }
+  if (isPan) {
+    console.log('panend');
+  }
+  if (isPress) {
+    console.log('pressend');
+  }
+
+  //console.log('end', point.clientX, point.clientY);
 };
 const cancel = (point) => {
+  clearTimeout(pressTimeoutHandle);
   console.log('cancel', point.clientX, point.clientY);
 };
