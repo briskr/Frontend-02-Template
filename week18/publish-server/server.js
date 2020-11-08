a4110f93bd86b36de216d2a771dfea5b5a2306c2
briskr <briskr@126.com>
2020-11-08 10:30:11 +0800

    发送前用archiver压缩、收到后用unzipper解开

--- week18/publish-server/server.js
+++ week18/publish-server/server.js
@@ -1,17 +1,22 @@
 const http = require('http');
 const fs = require('fs');
+const unzipper = require('unzipper');
 
 http
   .createServer(function (req, res) {
-    console.log('content-length', req.headers['content-length']);
-    const outFile = fs.createWriteStream('../server/public/index.html');
-    req.pipe(outFile);
+    console.debug('request received');
+    console.log('content-type', req.headers['content-type']);
+
+    // 读完 request 之后再发回响应，否则 pipe 过程没结束，文件收不完整
     req.on('end', () => {
-      console.log('request finished');
-      outFile.end();
+      res.end('success');
     });
 
-    // 保留发送响应
-    res.end('success');
+    // 输出到文件
+    //let outFile = fs.createWriteStream('../server/public/tmp.zip');
+    //req.pipe(outFile, { end: true });
+
+    // 输出到 unzipper ，解压出包里的内容
+    req.pipe(unzipper.Extract({ path: '../server/public/' }));
   })
   .listen(8082);
--- week18/publish-tool/publish.js
+++ week18/publish-tool/publish.js
@@ -1,33 +1,38 @@
 const http = require('http');
 const fs = require('fs');
+const archiver = require('archiver');
 
-const filename = './sample.html';
+// fs.stat(filename, function (err, stats) {
 
-fs.stat(filename, function (err, stats) {
-  const request = http.request(
-    {
-      //hostname: 'box',
-      hostname: 'localhost',
-      port: 8082,
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/octet-stream',
-        'Content-Length': stats.size,
-      },
+const request = http.request(
+  {
+    //hostname: 'box',
+    hostname: 'localhost',
+    port: 8082,
+    method: 'POST',
+    headers: {
+      //'Content-Type': 'application/octet-stream',
+      'Content-Type': 'application/zip',
+      //'Content-Length': stats.size,
     },
-    (res) => {
-      console.debug('response status:\n' + res.statusCode);
-      res.on('data', (chunk) => {
-        console.debug(chunk.toString());
-      });
-    }
-  );
+  },
+  (res) => {
+    console.debug('response status:\n' + res.statusCode);
+    res.on('data', (chunk) => {
+      console.debug(chunk.toString());
+    });
+  }
+);
 
-  const file = fs.createReadStream('./sample.html');
-  /* file.on('end', () => {
-    request.end();
-    console.debug('request sent');
-  }); */
-  // pipe 函数的选项 end: true 是默认值，上面的代码应该是没必要的
-  file.pipe(request, { end: true });
+// 压缩目录内容，作为 readable 流
+const archive = archiver('zip', {
+  zlib: { level: 9 },
 });
+archive.directory('./dist/', false);
+archive.finalize();
+
+//archive.pipe(fs.createWriteStream('tmp.zip'));
+
+archive.pipe(request);
+
+//});
