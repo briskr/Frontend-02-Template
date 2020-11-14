7a0fd36f5e6f0404dcc84b8ad06c93391b9f1464
briskr <briskr@126.com>
2020-11-14 16:25:15 +0800

    puppeteer示例

---
+++ week19/headless-demo/main.js
@@ -0,0 +1,15 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('http://localhost:8080/main.html');
+  const a = await page.$('div');
+  if (a) {
+    console.log(await a.asElement().boxModel());
+  }
+  const imgs = await page.$$('div');
+  debugger;
+
+  await browser.close();
+})();
